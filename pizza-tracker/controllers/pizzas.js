ed01b350dac02a5cc77c53fb96b6dcde05b05e23
Ian Clark <ianclark226@gmail.com>
2021-10-16 11:05:35 -0400

    connections

---
+++ pizza-tracker/controllers/pizzas.js
@@ -0,0 +1,6 @@
+// @desc Get all pizzas
+//@route GET /api/v1/pizzas
+
+exports.getPizzas = (req, res, next) => {
+  res.send('GET pizzas');
+}
---
+++ pizza-tracker/routes/pizzas.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+const { getPizzas, addPizza, deletePizza } = require('../controllers/pizzas');
+
+router.route('/').get(getPizzas)
+.post(addPizza);
+
+router.route('/:id')
+.delete(deletePizza);
+
+module.exports = router;
